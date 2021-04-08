caac23f6d1d034d5d0c31c3f1c4054bc6c7b75b3
kvartet <xyru1999@gmail.com>
2021-04-08 09:31:16 +0800

    tpe without while

---
+++ nni/algorithms/hpo/tpe.py
@@ -0,0 +1,315 @@
+import copy
+import random
+import logging
+import collections
+
+import numpy as np
+
+from schema import Optional, Schema
+from nni import ClassArgsValidator
+from nni.tuner import Tuner
+from nni.utils import NodeType, OptimizeMode, extract_scalar_reward, split_index
+
+from .utils import *
+
+
+_logger = logging.getLogger(__name__)
+
+
+'''
+tmp = {
+    "branch0":{
+        "_type":"choice",
+        "_value":[
+            {
+                "_name":"tmp1",
+                "batch_size":{"_type":"choice","_value":[1,2,3]},
+                "kernal_size":{"_type":"choice","_value":[1,2,3]},
+            },
+            {
+                "_name":"tmp2",
+                "batch_size":{"_type":"choice","_value":[1,2,3]},
+                "kernal_size":{"_type":"choice","_value":[1,2,3]},
+            }
+        ]
+    },
+    "branch1":{
+        "_type":"choice",
+        "_value":[
+            {
+                "_name":"tmp1",
+                "batch_size":{"_type":"choice","_value":[1,2,3]},
+                "kernal_size":{"_type":"choice","_value":[1,2,3]},
+            },
+            {
+                "_name":"tmp2",
+                "batch_size":{"_type":"choice","_value":[1,2,3]},
+                "kernal_size":{"_type":"choice","_value":[1,2,3]},
+            }
+        ]
+    }
+}
+'''
+
+
+class ParameterRange():
+    def __init__(self, name, algorithm_name, is_categorical, size,
+                 categorical_values, low, high, is_log_distributed, is_integer, aware):
+        self.name = name
+        self.tag = name if aware else algorithm_name + "|" + name
+        # self.tag = algorithm_name + "|" + name
+
+        self.is_categorical = is_categorical
+
+        self.size = size
+        self.categorical_values = categorical_values
+
+        self.low = low
+        self.high = high
+        self.is_log_distributed = is_log_distributed
+        self.is_integer = is_integer
+
+        if is_log_distributed:
+            self.high = np.log(high)
+            self.low = np.log(low)
+
+    @staticmethod
+    def categorical(algorithm_name, name, values, aware):
+        return ParameterRange(name, algorithm_name, True,
+                              len(values), values, np.nan, np.nan, False, False, aware)
+
+    @staticmethod
+    def numerical(algorithm_name, name, low, high, aware,
+                  is_log_distributed=False, is_integer=False):
+        return ParameterRange(name, algorithm_name, False,
+                              -1, None, low, high, is_log_distributed, is_integer, aware)
+
+
+class SearchSpace():
+    def __init__(self, json_string, aware):
+        self.is_nested = True if 'branch' in json_string.keys() else False
+        self.algorithms = collections.OrderedDict()
+        if self.is_nested:
+            self.pipelines = list()
+            for pipeline_json in json_string.values():
+                for algo_kv in pipeline_json["_value"]:
+                    self.pipelines.append(algo_kv["_name"])
+                    algo = list()
+                    self.algorithms[algo_kv["_name"]] = algo
+
+                    for param_kv in list(algo_kv.items())[1:]:
+                        param_name = param_kv[0]
+                        param_json = param_kv[1]
+                        param_type = param_json["_type"]
+                        if param_type == "choice":
+                            values = list()
+                            for val in param_json["_value"]:
+                                values.append(val)
+                            algo.append(ParameterRange.categorical(
+                                algo_kv["_name"], param_name, values, aware))
+                        else:
+                            values = param_json["_value"]
+                            low = values[0]
+                            high = values[1]
+
+                            log = (param_type ==
+                                "loguniform" or param_type == "qloguniform")
+                            integer = (
+                                param_type == "quniform" or param_type == "qloguniform")
+                            alog.append(ParameterRange.numerical(
+                                algo_kv["_name"], param_name, low, high, log, integer, aware))
+        else:
+            for name, value in json_string.items():
+                if value["_type"] == "choice":
+                    values = list()
+                    for val in value["_value"]:
+                        values.append(val)
+                    algo = (ParameterRange.categorical(name, name, values, aware))
+                    self.algorithms[name] = algo
+                else:
+                    print('Search space is error!')
+
+
+
+
+class Result():
+    def __init__(self, param_id, loss, param):
+        self.param_id = param_id
+        self.loss = loss
+        self.param = param
+
+
+class TPETuner(Tuner):
+    def __init__(self, aware=True, minimize_mode=False):
+        self.n_startup_jobs = 20
+        self.prior_weight = 1.0
+        self.oloss_gamma = 0.25
+        self.lf = 25
+        self.n_ei_candidates = 24
+        self.eps = 1e-12
+        self.rng = RandomNumberGenerator()
+
+        # self.space = SearchSpace(search_space)
+        self.minimize = minimize_mode
+        self.aware = aware
+
+        self.parameters = dict()  # Dictionary<int, TpeParameters>
+        self.running = set()
+        self.history = list()
+        self.lie = np.inf
+
+    def generate_parameters(self, parameter_id, **kwargs):
+        if (len(self.parameters) > self.n_startup_jobs and len(self.running) > 0):
+            fake_history = copy.deepcopy(self.history)
+            for item in self.running:
+                fake_history.append(
+                    Result(len(fake_history), self.lie, self.parameters[item]))
+            ret, param = self.suggest(self.space, fake_history)
+        else:
+            ret, param = self.suggest(self.space, self.history)
+
+        self.parameters[parameter_id] = param
+        self.running.add(parameter_id)
+
+        return ret
+
+    def update_search_space(self, search_space):
+        """
+        Update search space definition in tuner by search_space in parameters.
+
+        Will called when first setup experiemnt or update search space in WebUI.
+        """
+        self.space = SearchSpace(search_space, self.aware)
+
+        # return SearchSpace(search_space)
+
+    def receive_trial_result(self, parameter_id, parameters, loss, **kwargs):
+        if not self.minimize:
+            loss = - loss
+        self.running.remove(parameter_id)
+        self.lie = min(self.lie, loss)
+        self.history.append(
+            Result(parameter_id, loss, self.parameters[parameter_id]))
+
+    def suggest(self, space, history):
+        formatted_param = dict()
+        param = dict()  # Dictionary<string, double>
+
+        if self.space.is_nested:
+            pipeline_index = self.suggest_categorical(
+                history, "__pipeline__", len(self.space.pipelines))
+            chosen_pipeline = self.space.pipelines[pipeline_index]
+            param["__pipeline__"] = pipeline_index
+
+            for algo in self.space.algorithms.items():
+                if algo[0] in chosen_pipeline:
+                    formatted_algo = dict()
+                    formatted_param[algo[0]] = formatted_algo
+                    for param_range in algo[1]:
+                        if param_range.is_categorical:
+                            index = self.suggest_categorical(
+                                history, param_range.tag, param_range.size)
+                            # print('param_range.size:{}, index:{}'.format(param_range.size, index))
+                            param[param_range.tag] = index
+                            formatted_algo[param_range.name] = param_range.categorical_values[index]
+                        else:
+                            x = self.suggest_numerical(history, param_range.tag, param_range.low,
+                                                    param_range.high, param_range.is_log_distributed,
+                                                    param_range.is_integer)
+                            param[param_range.tag] = x
+                            formatted_algo[param_range.name] = param_range.is_integer if str(
+                                int(x)) else str(x)
+        # print('formatted_param:{}, param:{}'.format(formatted_param, param))
+        else:
+            for algo in self.space.algorithms.items():
+                if algo[1].is_categorical:
+                    index = self.suggest_categorical(history, algo[1].tag, algo[1].size)
+                    param[algo[1].tag] = index
+                    formatted_param[algo[1].name] = algo[1].categorical_values[index]
+        return formatted_param, param
+
+    def suggest_categorical(self, history, tag, size):
+
+        if len(history) < self.n_startup_jobs:
+            return self.rng.integer(size)
+
+        obs_below, obs_above = self.ap_split_trials(history, tag)
+
+        if len(obs_above) == 0 or len(obs_below) == 0:
+            return self.rng.integer(size)
+
+        weights = linear_forgetting_weights(len(obs_below), self.lf)
+        counts = np.bincount(obs_below, weights, size)
+        p = ((counts+self.prior_weight)/np.sum(counts+self.prior_weight))[:size]
+        # print('size:{}'.format(size))
+        # print('p_above:{}'.format(p))
+        sample = self.rng.categorical(p, self.n_ei_candidates)
+        # print('sample_below:{}'.format(sample))
+        below_llik = np.log([p[i] for i in sample])
+
+        weights = linear_forgetting_weights(len(obs_above), self.lf)
+        counts = np.bincount(obs_above, weights, size)
+        p = ((counts+self.prior_weight)/np.sum(counts+self.prior_weight))[:size]
+        # sample = self.rng.categorical(p, self.n_ei_candidates)
+        # print('p:{}'.format(p))
+        # print('sample:{}'.format(sample))
+        # for i in sample:
+        #     print(p[i],end=',')
+        above_llik = np.log([p[i] for i in sample])
+
+        return self.find_best(sample, below_llik, above_llik, size)
+
+    def suggest_numerical(self, history, tag, low, high, log, integer):
+        if len(history) < self.n_startup_jobs:
+            x = self.rng.uniform(low, high)
+            if log:
+                x = np.exp(x)
+            elif integer:
+                x = round(x)
+            return x
+
+        obs_below, obs_above = self.ap_split_trials(history, tag)
+
+        if log:
+            obs_below, obs_above = np.log(obs_below), np.log(obs_above)
+
+        prior_mu = 0.5 * (high + low)
+        prior_sigma = high - low
+
+        weights, mus, sigmas = adaptive_parzen_normal(
+            obs_below, self.prior_weight, prior_mu, prior_sigma)
+        samples = Gmm1(weights, mus, sigmas, low, high, log,
+                       integer, self.n_ei_candidates, self.rng)
+        below_llik = Gmm1_lpdf(samples, weights, mus,
+                               sigmas, low, high, log, integer)
+
+        weights, mus, sigmas = adaptive_parzen_normal(
+            obs_below, self.prior_weight, prior_mu, prior_sigma)
+        above_llik = Gmm1_lpdf(samples, weights, mus,
+                               sigmas, low, high, log, integer)
+
+        return find_best(samples, below_llik, above_llik)
+
+    def ap_split_trials(self, history, tag):
+        n_below = min(
+            int(np.ceil(self.oloss_gamma * np.sqrt(len(history)))), self.lf)
+        history_sorted = sorted(history, key=lambda x: x.loss)
+        # print('tag:{}'.format(tag))
+        # for item in history_sorted:
+        #     print('item.param.keys():{}'.format(item.param.keys()))
+        below = [item for item in history_sorted if tag in item.param.keys()][:n_below]
+        above = [item for item in history_sorted if tag in item.param.keys()][n_below:]
+        below_value = [item.param[tag] for item in sorted(below, key=lambda x: x.param_id)]
+        above_value = [item.param[tag] for item in sorted(above, key=lambda x: x.param_id)]
+        print('np.asarray(below_value):{}, np.asarray(above_value):{}'\
+            .format(np.asarray(below_value), np.asarray(above_value)))
+        return np.asarray(below_value), np.asarray(above_value)
+
+    def find_best(self, samples, below_llik, above_llik, size):
+        llik = below_llik - above_llik
+        best = np.argmax(llik)
+        while samples[best] >= size:
+            # print('best:{}, samples[best]:{}'.format(best, samples[best]))
+            llik[best] = - np.inf
+            best = np.argmax(llik)
+        return samples[best]
---
+++ nni/algorithms/hpo/utils.py
@@ -0,0 +1,241 @@
+import numpy as np
+
+from scipy.special import erf
+from past.utils import old_div
+
+
+class RandomNumberGenerator():
+    def __init__(self):
+        np.random.seed(13) # 7 9 13
+
+    def integer(self, high):
+        return np.random.randint(0, high)
+
+    def uniform(self, low, high):
+        return np.ramdom.uniform(low, high)
+
+    def normal(self, mu, sigma):
+        return np.random.gauss(mu, sigma)
+
+    def categorical(self, possibility, size=1):
+        return np.argmax(np.random.multinomial(1, possibility, (size,)), axis=1)[0] if size==1 else np.argmax(np.random.multinomial(1, possibility, (size,)), axis=1)
+
+
+# def linear_forgetting_weights(n, lf):
+#     weights = [1.0] * n
+#     ramp_start = 1.0 / n
+#     ramp_length= n - lf
+#     if ramp_length == 1:
+#         weights[0] = ramp_start
+#     else:
+#         for i in range(ramp_length):
+#             weights[i] = ramp_start + (1.0 - ramp_start) / (ramp_length - 1) * i
+#     return np.asarray(weights)
+
+def linear_forgetting_weights(N, LF):
+    assert N >= 0
+    assert LF > 0
+    if N == 0:
+        return np.asarray([])
+    if N < LF:
+        return np.ones(N)
+    ramp = np.linspace(old_div(1.0, N), 1.0, num=N - LF)
+    flat = np.ones(LF)
+    weights = np.concatenate([ramp, flat], axis=0)
+    assert weights.shape == (N,), (weights.shape, N)
+    return weights
+
+def adaptive_parzen_normal(mus, prior_weight, prior_mu, prior_sigma):
+
+    mus_orig = np.array(mus)
+    mus = np.array(mus)
+    assert str(mus.dtype) != "object"
+
+    if mus.ndim != 1:
+        raise TypeError("mus must be vector", mus)
+    if len(mus) == 0:
+        mus = np.asarray([prior_mu])
+        sigma = np.asarray([prior_sigma])
+    elif len(mus) == 1:
+        mus = np.asarray([prior_mu] + [mus[0]])
+        sigma = np.asarray([prior_sigma, prior_sigma * 0.5])
+    elif len(mus) >= 2:
+        order = np.argsort(mus)
+        mus = mus[order]
+        sigma = np.zeros_like(mus)
+        sigma[1:-1] = np.maximum(mus[1:-1] - mus[0:-2], mus[2:] - mus[1:-1])
+        if len(mus) > 2:
+            lsigma = mus[2] - mus[0]
+            usigma = mus[-1] - mus[-3]
+        else:
+            lsigma = mus[1] - mus[0]
+            usigma = mus[-1] - mus[-2]
+
+        sigma[0] = lsigma
+        sigma[-1] = usigma
+
+        mus[order] = mus.copy()
+        sigma[order] = sigma.copy()
+
+        if not np.all(mus_orig == mus):
+            print("orig", mus_orig)
+            print("mus", mus)
+        assert np.all(mus_orig == mus)
+
+        # put the prior back in
+        mus = np.asarray([prior_mu] + list(mus))
+        sigma = np.asarray([prior_sigma] + list(sigma))
+
+    maxsigma = prior_sigma
+    # -- magic formula:
+    minsigma = old_div(prior_sigma, np.sqrt(1 + len(mus)))
+
+    sigma = np.clip(sigma, minsigma, maxsigma)
+
+    weights = np.ones(len(mus), dtype=mus.dtype)
+    weights[0] = prior_weight
+
+    weights = old_div(weights, weights.sum())
+
+    return weights, mus, sigma
+
+
+def Gmm1(weights, mus, sigmas, low, high, log, integer, n_ei_candidates, rng):
+    samples = [0] * n_ei_candidates
+    for i in range(n_ei_candidates):
+        while True:
+            active = rng.categorical(weights)
+            draw = rng.normal(mus[active], sigmas[active])
+            if draw < low or draw >= high:
+                continue
+            if log:
+                draw = np.exp(draw)
+            if integer:
+                draw = np.round(draw)
+            samples[i] = draw
+            break
+        return np.asarray(samples)
+
+def Gmm1_lpdf(samples, weights, mus, sigmas, low, high, log, integer, rng):
+    print('unuseless')
+    return weights
+    # print()
+    # samples, weights, mus, sigmas = list(
+    #     map(np.asarray, (samples, weights, mus, sigmas))
+    # )
+
+    # assert len(weights) == len(mus) == len(sigmas)
+    # samples = _samples.flatten()
+
+    # p_accept = np.sum(
+    #     weights * (normal_cdf(high, mus, sigmas) -
+    #                 normal_cdf(low, mus, sigmas))
+    # )
+
+    # # ret = [0] * len(samples)
+    # # for i in range(len(samples)):
+    # if not integer:
+    #     if log:
+    #         ret = logsum_rows(lognormal_lpdf(samples, weights, mus, sigmas))
+    #     else:
+    #         ret = logsum_rows(lognormal_lpdf(samples, weights, mus, sigmas, p_accept))
+    # else:
+    #     prob = 0
+    #     if log:
+    #         ubound = np.log(np.minimum(samples + 0.5, np.exp(high)))
+    #         lbound = np.log(np.maximum(samples - 0.5, np.exp(low)))
+    #     else:
+    #         ubound = np.minimum(samples + 0.5, high)
+    #         lobund = np.maximum(samples - 0.5, low)
+    #     prob += np.sum(weights * normal_cdf(ubound, mus, sigmas))
+    #     prob -= np.sum(weights * normal_cdf(lbound, mus, sigmas)
+    #     ret = np.log(prob) - np.log(p_accept)
+
+    # return ret   
+
+
+# def Gmm1_lpdf(samples, weights, mus, sigmas, low, high, log, integer, rng):
+#     samples, weights, mus, sigmas = list(
+#         map(np.asarray, (samples, weights, mus, sigmas))
+#     )
+
+#     assert len(weights) == len(mus) == len(sigmas)
+#     _samples = samples
+#     samples = _samples.flatten()
+
+#     p_accept = np.sum(
+#         weights * (normal_cdf(high, mus, sigmas) -
+#                     normal_cdf(low, mus, sigmas))
+#     )
+
+#     ret = [0] * len(samples)
+#     for i in range(len(samples)):
+#         if not integer:
+#             if log:
+#                 ret[i] = logsum_rows(lognormal_lpdf(samples[i], weights, mus, sigmas))
+#             else:
+#                 ret[i] = logsum_rows(lognormal_lpdf(samples[i], weights, mus, sigmas, p_accept))
+#         else:
+#             prob = 0
+#             if log:
+#                 ubound = np.log(np.min(samples[i] + 0.5, np.exp(high)))
+#                 lbound = np.log(np.max(samples[i] - 0.5, np.exp(low)))
+#             else:
+#                 ubound = np.min(samples[i] + 0.5, high)
+#                 lobund = np.max(samples[i] - 0.5, low)
+#             prob += np.sum(weights * normal_cdf(ubound, mus, sigmas))
+#             prob -= np.sum(weights * normal_cdf(lbound, mus, sigmas)
+#             ret[i] = np.log(prob) - np.log(p_accept)
+
+#     return np.asarray(ret)    
+
+
+
+
+def normal_cdf(x, mu, sigma):
+    top = x - np.asarray(mu)
+    bottom = np.maximum(np.sqrt(2) * sigma, EPS)
+    z = 0.5 + 0.5 * erf(top/bottom)
+    return 0.5 * (1 + erf(z))
+
+
+def lognormal_lpdf(x, mu, sigma):
+    # formula copied from wikipedia
+    # http://en.wikipedia.org/wiki/Log-normal_distribution
+    assert np.all(sigma >= 0)
+    sigma = np.maximum(sigma, EPS)
+    Z = sigma * x * np.sqrt(2 * np.pi)
+    E = 0.5 * (old_div((np.log(x) - mu), sigma)) ** 2
+    rval = -E - np.log(Z)
+    return rval
+
+
+def qlognormal_lpdf(x, mu, sigma, q):
+    # casting rounds up to nearest step multiple.
+    # so lpdf is log of integral from x-step to x+1 of P(x)
+
+    # XXX: subtracting two numbers potentially very close together.
+    return np.log(lognormal_cdf(x, mu, sigma) - lognormal_cdf(x - q, mu, sigma))
+
+
+def logsum_rows(x):  # logsum
+    m = x.max(axis=1)
+    return np.log(np.exp(x - m[:, None]).sum(axis=1)) + m
+
+
+# def bin_count(x, weights, min_length):
+#     ret = [0] * max(np.max(x), len(x), min_length)
+#     print('x:{}, weights:{}, min_length:{}'.format(x, weights, min_length))
+#     for i in range(min(len(x), min_length, len(weights))):
+#         print('i:{}, x[i]:{}'.format(i,x[i]))
+#         ret[x[i]] += weights[i]
+#     return np.asarray(ret[:min_length])
+
+# def bin_count(x, weights=None, minlength=None, p=None):
+#     y = np.asarray(x, dtype="int")
+#     # hack for pchoice, p is passed as [ np.repeat(p, obs.size) ],
+#     # so scope.len(p) gives incorrect #dimensions, need to get just the first one
+#     if p is not None and p.ndim == 2:
+#         assert np.all(p == p[0])
+#         minlength = len(p[0])
+#     return np.bincount(y, weights, minlength)
